a2015f84f5eb0a756610c8a22f8620fb577a1caa
Arsen <arsen.khadikov@gmail.com>
2016-03-13 21:23:31 +0200

    second commit

--- WEEK5/primitive_calculator/primitive_calculator.py
+++ WEEK5/primitive_calculator/primitive_calculator.py
@@ -1,6 +1,6 @@
 # Uses python3
 import sys
-
+'''
 def optimal_sequence(n):
     if n == 1:
         return [1]
@@ -38,6 +38,42 @@ def optimal_sequence(n):
             sequence.append(sequence[-1]*2)
         elif new_array[-(i+1)] == 3:
             sequence.append(sequence[-1]*3)
+    return sequence
+
+# Uses python3
+import sys
+'''
+def optimal_sequence(n):
+    if n == 1:
+        return [1]
+    operations = [0, 0]
+    which_op = []
+    for w in range(2, n + 1):
+        operation = operations[w - 1]
+        operation += 1
+        wh = 1
+        if w > 1:
+            op = 1 + w % 2 + operations[int(w/2)]
+            if op < operation:
+                operation = op
+                wh = 2
+        if w > 2:
+            op = 1 + w % 3 + operations[int(w/3)]
+            if op < operation:
+                operation = op
+                wh = 3
+        which_op.append(wh)
+        operations.append(operation)
+    x = n
+    sequence = [x]
+    while x > 1:
+        if which_op[x-2] == 3:
+            x = int(x/3)
+        elif which_op[x-2] == 2:
+            x = int(x/2)
+        elif which_op[x-2] == 1:
+            x -= 1
+        sequence.append(x)
     return sequence
 
 input = sys.stdin.read()
