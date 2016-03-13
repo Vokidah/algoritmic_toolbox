9f48637387ff1aa881aabff3f17340c63d1686ee
Arsen <arsen.khadikov@gmail.com>
2016-03-13 21:24:36 +0200

    third commit

--- WEEK5/primitive_calculator/primitive_calculator.py
+++ WEEK5/primitive_calculator/primitive_calculator.py
@@ -1,48 +1,6 @@
 # Uses python3
 import sys
-'''
-def optimal_sequence(n):
-    if n == 1:
-        return [1]
-    operations = [0, 0]
-    which_op = []
-    for w in range(2, n + 1):
-        operation = operations[w - 1]
-        operation += 1
-        wh = 1
-        if w > 1:
-            op = 1 + w % 2 + operations[int(w/2)]
-            if op < operation:
-                operation = op
-                wh = 2
-        if w > 2:
-            op = 1 + w % 3 + operations[int(w/3)]
-            if op < operation:
-                operation = op
-                wh = 3
-        which_op.append(wh)
-        operations.append(operation)
-    operations.remove(operations[0])
-    operations.remove(operations[0])
-    j = operations[-1]
-    new_array = []
-    for i in range(0, len(operations)):
-        if operations[-(i+1)] == j:
-            j -= 1
-            new_array.append(which_op[-(i+1)])
-    sequence = [1]
-    for i in range(0, len(new_array)):
-        if new_array[-(i+1)] == 1:
-            sequence.append(sequence[-1]+1)
-        elif new_array[-(i+1)] == 2:
-            sequence.append(sequence[-1]*2)
-        elif new_array[-(i+1)] == 3:
-            sequence.append(sequence[-1]*3)
-    return sequence
 
-# Uses python3
-import sys
-'''
 def optimal_sequence(n):
     if n == 1:
         return [1]
